2edf3bdcced0b5cd6258c9365d7780304305a571
Adrian Habryka <adrian.habryka@gmail.com>
2023-08-22 09:59:00 +0200

    Add custom route matcher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, UrlSegment } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        matcher: (url) => {
+          if (url.length === 1 && url[0].path.match(/^@[\w]+$/gm)) {
+            return {
+              consumed: url,
+              posParams: {
+                username: new UrlSegment(url[0].path.slice(1), {})
+              }
+            };
+          }
+      
+          return null;
+        },
+        component: ProfileComponent
+      }
+    ])],
   providers: [],
   bootstrap: [AppComponent]
 })
